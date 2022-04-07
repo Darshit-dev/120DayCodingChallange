0b9c74bd3b977fb05da6c93bc9d44c20d58ce4e7
Darshit-dev <khenidarshit@gmail.com>
2022-04-07 22:20:36 +0530

    day-18 assignment completed

--- src/routes/route.js
+++ src/routes/route.js
@@ -11,6 +11,47 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+router.get('/movies/:indexNumber', function (req, res) {
+    let mArr = ["rand de basnasti", "the shining", "lord of the rings", "bartman begins"]
+
+    let i = req.params.indexNumber
+
+    let catchMovie;
+    if ((i - 1) < mArr.length) {
+        catchMovie = mArr[i - 1]
+    } else {
+        catchMovie = "please enter number within" + mArr.length
+    }
+    res.send(catchMovie)
+
+});
+
+router.get('/films/:filmID',function(req,res){
+    // how to send both response together in a single request
+    const filmArr = [{
+        id: 1,
+        name: "The Shining"
+    }, {
+        id: 2,
+        name: "Incendies"
+    }, {
+        id: 3,
+        name: "Rang de Basanti"
+    }, {
+        id: 4,
+        name: "Finding Nemo"
+    }]
+    console.log(filmArr)
+    //res.send(filmArr[req.params.filmID-1])
+    let catchFilm;  //why i am not able to use const ?
+    if ((i - 1) < filmArr.length) {
+        catchFilm = mArr[i - 1]
+    } else {
+        catchFilm = "please enter number within" + filmArr.length
+    }
+    res.send(catchFilm)
+
+})
 
 
 
