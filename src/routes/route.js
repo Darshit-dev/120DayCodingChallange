1d104da2693d95e76fdbafb8f9387eca37b72de5
Darshit-dev <khenidarshit@gmail.com>
2022-04-19 23:44:13 +0530

    assignment done

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,12 +4,23 @@ const router = express.Router();
 //importing module
 const allController = require('../controllers/allController')
 
+const mid = function(req,res,next){
+    let logging =true
+    if(logging){
+        let mydate = new Date()
+        console.log("your date is :" + mydate + "and your ip is " + req.ip)
+        next()
+    }
+    else{
+        res.send("mds : Please logging first")
+    }
+    
+}
 
 
-
-router.post('/createBatch',allController.createBatch)
-router.post('/createDev',allController.createDev)
-router.get('/developers',allController.devFromBatch)
-router.get('/scholership',allController.scholership)
+router.post('/createBatch',mid,allController.createBatch)
+router.post('/createDev',mid,allController.createDev)
+router.get('/developers',mid,allController.devFromBatch)
+router.get('/scholership',mid,allController.scholership)
 
 module.exports = router;
